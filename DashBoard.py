e881a65f79fb44ee03dbf7c8f59b876e17c74448
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-20 18:28:32 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -5,7 +5,7 @@ Created on Fri May 19 17:18:17 2023
 @author: ALEJANDRO
 """
 import streamlit as st
-from streamlit_folium import folium_static
+from streamlit_folium import folium
 import folium
 
 m = folium.Map (location=[39.949610, -75.150282], zoom_start=16)
