147b1255c990249c033af8e041701710c4d24f12
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-20 18:09:19 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -4,43 +4,15 @@ Created on Fri May 19 17:18:17 2023
 
 @author: ALEJANDRO
 """
-#Librerias
-# importar Folium
 import streamlit as st
-from streamlit_folium import st_folium()
+from streamlit_folium import st_folium
 import folium
-import pandas as pd
-#import webbrowser
 
-from folium import plugins
-from folium.plugins.marker_cluster import MarkerCluster # Agrega los puntos espaciales
+m = folium.Map (location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282],
+    popup="Liberty Bell",
+    tooltip="Liberty Bell"
+    ).add_to(m)
 
-#from folium.folium import Map
-#from folium.map import Marker
-
-# importar streamlit para diagramar la APP
-
-
-APP_TITLE = 'DASHBOARD MONITOREO DE SOLIDOS - TOOLS AND TESTING'
-APP_SUB_TITLE = 'VER 1.0'
-
-def main():
-    st.set_page_config(APP_TITLE)
-    st.title('DASHBOARD MONITOREO DE SOLIDOS - TOOLS AND TESTING')
-    st.caption('VER 1.0')
-    
-    # LOAD DATA
-    # -- lee los datos del archivo de excel 
-    pozos = pd.read_excel('C:/Users/ALEJANDRO/Downloads/pozos.xlsx')
-    
-    st.write(pozos.shape)
-    st.write(pozos.Head())
-    st.write(pozos.columns)
-    
-    # DISPLAY FILTERS AND MAP
-    
-    
-    # DISPLAY METRICS
-        
-if __name__ == "_main_":
-    main()
+st_data = st_folium(m, width = 725)
