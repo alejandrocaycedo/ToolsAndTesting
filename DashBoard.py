82f57cb9a2bd9151b197e06ccdf13c3c24101742
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-21 08:11:50 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -4,13 +4,45 @@ Created on Fri May 19 17:18:17 2023
 
 @author: ALEJANDRO
 """
-import streamlit as st
-from streamlit_folium import folium
+#Librerias
+# importar Folium
 import folium
+import pandas as pd
+import webbrowser
 
-m = folium.Map (location=[39.949610, -75.150282], zoom_start=16)
-folium.Marker(
-    [39.949610, -75.150282],
-    popup="Liberty Bell",
-    tooltip="Liberty Bell"
-    ).add_to(m)
+from folium import plugins
+from folium.plugins.marker_cluster import MarkerCluster # Agrega los puntos espaciales
+
+#from folium.folium import Map
+from folium.map import Marker
+
+# importar streamlit para diagramar la APP
+import streamlit as st
+
+APP_TITLE = 'Monitoreo de arrastre de sólidos - TOOLS AND TESTING'
+APP_SUB_TITLE = 'Ver 1.0 - todos los derechos reservados'
+
+#def main():
+st.set_page_config('Tools and testing - 2023')   
+st.title(APP_TITLE)
+st.caption(APP_SUB_TITLE)
+    
+    #Load Data
+pozos = pd.read_excel('https://github.com/alejandrocaycedo/ToolsAndTesting/blob/main/Pozos.xlsx')
+st.write(pozos.head())
+
+    
+    # DISPLAY FILTERS AND MAP
+    
+    
+    # DISPLAY METRICS
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "_main_":
+    main()
