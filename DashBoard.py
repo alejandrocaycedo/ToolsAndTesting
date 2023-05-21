244befa3ae62fbac5827b65737f4a5b59f3fc3de
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-21 08:37:56 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -28,7 +28,7 @@ st.title(APP_TITLE)
 st.caption(APP_SUB_TITLE)
     
     #Load Data
-pozos = pd.read_excel('/app/toolsandtesting/Pozos.xlsx')
+pozos = pd.read_excel('/app/toolsandtesting/Pozos.csv')
 st.write(pozos.head())
 
     
