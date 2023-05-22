4366ecbff8e1e06531cc8538771fc9e3e1d5cd4a
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-22 08:03:35 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -8,7 +8,7 @@ Created on Fri May 19 17:18:17 2023
 # importar Folium
 import folium
 import pandas as pd
-import webbrowser
+#import webbrowser
 
 from folium import plugins
 from folium.plugins.marker_cluster import MarkerCluster # Agrega los puntos espaciales
@@ -18,6 +18,7 @@ from folium.map import Marker
 
 # importar streamlit para diagramar la APP
 import streamlit as st
+from streamlit_folium import st_folium
 
 APP_TITLE = 'Monitoreo de arrastre de sólidos - TOOLS AND TESTING'
 APP_SUB_TITLE = 'Ver 1.0 - todos los derechos reservados'
@@ -33,7 +34,9 @@ st.write(pozos.head())
 
     
     # DISPLAY FILTERS AND MAP
-    
+mapa = folium.Map(location = [7.10, -73.98],
+                     zoom_start = 12)
+st_mapa = st_folium(mapa)
     
     # DISPLAY METRICS
     
