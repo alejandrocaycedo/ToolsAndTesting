f0adba3b14daad3ff1378d97c76db350a966ea4e
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-22 11:52:52 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -66,7 +66,7 @@ for i in range(len(PPMm)):
     NorPPMm = PPMm[i]/MaxPPMm
     
 for i in range(len(Latitud)):
-    DataHeat = [float(Latitud[i]), float(Longitud[i]), float(NorPPMm[i])]
+    DataHeat[i] = [float(Latitud[i]), float(Longitud[i]), float(NorPPMm[i])]
         # DISPLAY FILTERS AND MAP
 mc_pozos = MarkerCluster()
 mapa = folium.Map(location = [7.10, -73.98],
