b67b0eb7c2a9e48f471a2421c49a7e500015f1a9
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-22 08:05:09 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -37,7 +37,38 @@ st.write(pozos.head())
 mapa = folium.Map(location = [7.10, -73.98],
                      zoom_start = 12)
 st_mapa = st_folium(mapa)
-    
+nombre = list(pozos["NamePozo"])
+Latitud = list(pozos["Latitud"])
+Longitud = list(pozos["Longitud"])
+BWPD = list(pozos["BWPD"])
+BOPD = list(pozos["BOPD"])
+BFPD = list(pozos["BFPD"])
+BSW = list(pozos["BSW"])
+PPMp = list(pozos["PPMp"])
+PPMm = list(pozos["PPMm"])
+um25to45 = list(pozos["25_45um"])
+um45to106 = list(pozos["45_106um"])
+um106to212 = list(pozos["106_212um"])
+um212to425 = list(pozos["212_425um"])
+um425 = list(pozos["mayor_425um"])
+color_i = list(pozos["color_icono"])
+prefijo = list(pozos["Prefijo"])
+Ticono = list(pozos["icono"])
+
+mc_pozos = MarkerCluster()
+
+#Pozos al mapa
+for nomb,lat,lon, bw, bo, bf, bs, PMp, PMm, um25, um45, um106, um212, um42, col_i, pref, Tico in zip(nombre, Latitud, Longitud, BWPD, BOPD, BFPD, BSW, PPMp, PPMm, um25to45, um45to106, um106to212, um212to425, um425, color_i, prefijo, Ticono):
+    mc_pozos.add_child(folium.Marker(location=[lat,lon],
+    popup= "<b> Nombre: </b>" +str(nomb)+ "<br><b> PPM Promedio: <b>" +str(PMp)+"<br>", max_width=4000, min_width=4000,
+    icon=folium.Icon(color=col_i,
+    icon_color="blue",
+    icon=Tico,
+    prefix=pref)))           
+
+capa_pozos = folium.FeatureGroup(name="pozos")
+mc_pozos.add_to(capa_pozos)
+mapa.add_child(capa_pozos)
     # DISPLAY METRICS
     
     
