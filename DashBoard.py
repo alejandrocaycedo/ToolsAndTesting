0f2e3d3b8891da3b33dba2c64163cfc645938f1f
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-23 12:10:45 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -112,7 +112,7 @@ with columns [0]:
     st.write(MaxPPMp)
 
 with columns [1]:
-    st.write("Mapa de calor valores PPM promedio y Ubicación de los pozos ")
+    st.write("Mapa de calor, valores PPM promedio y Ubicación de los pozos ")
     st_mapa = st_folium(mapa, width=1024, height=800)
 
     # DISPLAY METRICS
