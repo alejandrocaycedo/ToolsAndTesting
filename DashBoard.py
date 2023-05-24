322245a5fcd32ae7e976eac3e1312587c56ae948
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-24 15:12:53 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -94,7 +94,30 @@ mapa = folium.Map(location = [7.10, -73.98],
 #Pozos al mapa
 for nomb,lat,lon, bw, bo, bf, bs, PMp, PMm, um25, um45, um106, um212, um42, col_i, pref in zip(nombre, Latitud, Longitud, BWPD, BOPD, BFPD, BSW, PPMp, PPMm, um25to45, um45to106, um106to212, um212to425, um425, color_i, prefijo):
     mc_pozos.add_child(folium.Marker(location=[float(lat),float(lon)],
-    popup= "<b> Nombre: </b>" +str(nomb)+ "<br><b> PPM Promedio: <b>" +str(PMp)+"<br>", max_width=4000, min_width=4000,
+    popup= """f
+    <!DOCTYPE html>
+    <html>
+        <head>
+        <title> Datos del Pozo </title>
+        </head>
+        <body>
+        <table border = "3" cellpadding = "5" cellspacing ="5">
+        <tr>
+        <tr>
+            <th style="text-align: center; background-color:#ccccff"> Pozo:</th>
+            <th style="text-align: center; background-color:#ccccff"> BWPD:</th>
+            <th style="text-align: center; background-color:#ccccff"> BSW:</th>
+            <th style="text-align: center; background-color:#ccccff"> PPM(prom):</th>
+        </tr>
+            <td style="text-align: center">(nomb)</td>
+            <td style="text-align: center">(bw)</td>
+            <td style="text-align: center">(bs)</td>
+            <td style="text-align: center">(PMp)</td>
+        </tr>
+        </table>
+        </body>
+        </html>
+    """ ,                                                                               #"<b> Nombre: </b>" +str(nomb)+ "<br><b> PPM Promedio: <b>" +str(PMp)+"<br>", max_width=4000, min_width=4000,
     icon=folium.Icon(color=col_i,
     icon_color="blue",
     icon="tower-observation",
