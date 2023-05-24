903f141cd13b38675c86e4f8070eb47698bdc9d4
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-24 15:23:06 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -94,7 +94,7 @@ mapa = folium.Map(location = [7.10, -73.98],
 #Pozos al mapa
 for nomb,lat,lon, bw, bo, bf, bs, PMp, PMm, um25, um45, um106, um212, um42, col_i, pref in zip(nombre, Latitud, Longitud, BWPD, BOPD, BFPD, BSW, PPMp, PPMm, um25to45, um45to106, um106to212, um212to425, um425, color_i, prefijo):
     mc_pozos.add_child(folium.Marker(location=[float(lat),float(lon)],
-    popup= """f
+    popup= """
     <!DOCTYPE html>
     <html>
         <head>
