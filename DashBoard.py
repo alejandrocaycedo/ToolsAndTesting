fc36309333e6259777267a025e876ab7579c5c7e
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-25 12:02:57 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -25,12 +25,11 @@ st.caption(APP_SUB_TITLE)
 
 st.sidebar.subheader('Tipo de análisis')
 page_selection = st.sidebar.selectbox('Seleccionar tipo de análisis', ['Mapa de calor Concentración de sólidos por campo', 'Historico de concentración de sólidos por pozo','Estimaciones por pozo'])
-pages_main = {
-              'Mapa de calor Concentración de sólidos por campo' : Func_MapaCalor() ,
+pages_main = {'Mapa de calor Concentración de sólidos por campo' : Func_MapaCalor() ,
               'Historico de concentración de sólidos por pozo' : run_Historico ,
               'Estimaciones por pozo': run_Estimaciones
     }
-
+pages_main[page_selection]()
 # abre pagina seleccionada
 st.sidebar.title("Selecione análisis")
 
