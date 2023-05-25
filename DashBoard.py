41a126ba4d19907167d29926150c9ead553d8766
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-05-25 10:45:29 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -35,10 +35,10 @@ st.caption(APP_SUB_TITLE)
 
 #Configuración de la pagina
 
-hide_footer_style = "'
+hide_footer_style = """
 <style>
 .reportview-container .main footer {visibility: hidden;}
-'"
+"""
 st.markdown(hide_footer_style, unsafe_allow_html=True)
 
 st.sidebar.title("Consultas")
