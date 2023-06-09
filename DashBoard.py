dfc0b895ce09d34089f5896c0ffb675c7a192b89
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-06-09 07:30:03 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -25,7 +25,7 @@ col1, col2 = st.columns(2)
 
 # Conectar a la base de datos
 con = sqlite3.connect("/app/toolsandtesting/DataBase/Database.db") 
-pf = pd.read_sql_query("SELECT * from Coordenadas", con)
+pf = pd.read_sql_query("SELECT * from Info_Monitoreo", con)
 with col1:
     st.write(heat)    
 
