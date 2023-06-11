ae5408df21b0c9522cbcaf196b78fc8d41da2e75
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-06-11 13:00:02 -0500

    Update DashBoard.py

--- DashBoard.py
+++ DashBoard.py
@@ -119,8 +119,9 @@ BSWbsw = list(prodBSW["BSW"])
 
 posicion = [0]
 for x, y, namepozo in zip(X, Y, nombre):
-    p1=pyproj.Proj(proj='utm', zone=17, ellps='WGS84', preserve_units=False)
+    p1 = pyproj.Proj(proj='utm', zone=17, ellps='WGS84', preserve_units=False)
     (lon,lat)=p1(x, y, inverse=True)
+    st.write(lon , lat)  
     posicion.append([namepozo, (lat - ajusteLatitud), (lon - ajusteLogitud)])
 
 with col1:
