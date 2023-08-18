7c2bbf20e56c3d7e54c351aff1cd845e15ad599a
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-08-18 11:25:34 -0500

    Create 4_Estimaciones_Pozos.py

---
+++ Streamlit/pages/4_Estimaciones_Pozos.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 11:46:54 2023
+
+@author: ALEJANDRO
+"""
+
+import streamlit as st
+
+# SideBar
+st.sidebar.title("Tools and Testing - 2023")
+st.title("Estimaciones")
