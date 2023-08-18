227f459c9b9aabc23384450dd0f668eddafd5ac1
alejandrocaycedo <alejandro.caycedo.75@gmail.com>
2023-08-18 11:25:29 -0500

    Create 2_Historico_Pozos.py

---
+++ Streamlit/pages/2_Historico_Pozos.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 11:42:52 2023
+
+@author: ALEJANDRO
+"""
+import streamlit as st
+
+# SideBar
+st.sidebar.title("Tools and Testing - 2023")
+st.title("Historico")
